a941fcc628a134b303d6e2b4369e5ab736eeac6a
Kevin Kennedy <6423774+KevinKennedy@users.noreply.github.com>
2024-03-28 09:18:37 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Parallel.ForEach(Enumerable.Range(1, 100000000), num =>
+        {
+            Fibonacci(Math.Min(num, 15000));
+        });
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
